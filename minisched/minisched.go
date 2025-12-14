d6250875390979e91abbd2ad975e59937f19d5ca
Shinra Yamakawa <s.yamakawa@flatt.tech>
2025-12-14 12:49:54 +0900

    fix: forget calling addAllEventHandlers() and setting Taget during Binding

--- minisched/minisched.go
+++ minisched/minisched.go
@@ -9,6 +9,7 @@ import (
 
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
 
 	"github.com/OldBigBuddha/mini-kube-scheduler/minisched/queue"
 	"k8s.io/client-go/informers"
@@ -30,10 +31,13 @@ func New(
 		client:          client,
 	}
 
+	addAllEventHandlers(sched, informerFactory)
+
 	return sched
 }
 
 func (sched *Scheduler) Run(ctx context.Context) {
+	wait.UntilWithContext(ctx, sched.scheduleOne, 0)
 }
 
 func (sched *Scheduler) scheduleOne(ctx context.Context) {
@@ -49,6 +53,7 @@ func (sched *Scheduler) scheduleOne(ctx context.Context) {
 	// pick a random node
 	rand.Seed(time.Now().UnixNano())
 	selectedNode := nodes.Items[rand.Intn(len(nodes.Items))]
+	klog.Infof("minischeduler: Selected node name: %s", selectedNode.Name)
 
 	// bind pod to node
 	if err := sched.Bind(ctx, pod, selectedNode.Name); err != nil {
@@ -58,15 +63,22 @@ func (sched *Scheduler) scheduleOne(ctx context.Context) {
 }
 
 func (sched *Scheduler) Bind(ctx context.Context, p *v1.Pod, nodeName string) error {
+	klog.Infof("minischeduler: Try to bind %s/%s to %s", p.Namespace, p.Name, nodeName)
 	binding := &v1.Binding{
 		ObjectMeta: metav1.ObjectMeta{
 			Namespace: p.Namespace,
 			Name:      p.Name,
 			UID:       p.UID,
 		},
+		Target: v1.ObjectReference{
+			Kind: "Node",
+			Name: nodeName,
+		},
 	}
 
-	if err := sched.client.CoreV1().Pods(binding.Namespace).Bind(ctx, binding, metav1.CreateOptions{}); err != nil {
+	if err := sched.client.CoreV1().
+		Pods(binding.Namespace).
+		Bind(ctx, binding, metav1.CreateOptions{}); err != nil {
 		return err
 	}
 
