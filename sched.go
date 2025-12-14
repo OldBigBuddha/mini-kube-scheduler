07768dd77e3e82ac33fdddc95bde34aa7cdde107
Shinra Yamakawa <s.yamakawa@flatt.tech>
2025-12-14 12:50:31 +0900

    chore: fork scenario

--- sched.go
+++ sched.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"strconv"
+	"time"
+
 	"github.com/OldBigBuddha/mini-kube-scheduler/scheduler"
 	"golang.org/x/xerrors"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	clientset "k8s.io/client-go/kubernetes"
 	"k8s.io/klog/v2"
 
@@ -61,5 +68,49 @@ func start() error {
 }
 
 func scenario(client clientset.Interface) error {
+	ctx := context.Background()
+
+	// create node0 ~ node9
+	for i := 0; i < 10; i++ {
+		suffix := strconv.Itoa(i)
+		_, err := client.CoreV1().Nodes().Create(ctx, &v1.Node{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "node" + suffix,
+			},
+		}, metav1.CreateOptions{})
+		if err != nil {
+			return fmt.Errorf("create node: %w", err)
+		}
+	}
+
+	klog.Info("scenario: all nodes created")
+
+	_, err := client.CoreV1().Pods("default").Create(ctx, &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{Name: "pod1"},
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				{
+					Name:  "container1",
+					Image: "k8s.gcr.io/pause:3.5",
+				},
+			},
+		},
+	}, metav1.CreateOptions{})
+	if err != nil {
+		return fmt.Errorf("create pod: %w", err)
+	}
+
+	klog.Info("scenario: pod1 created")
+
+	// wait to schedule
+	time.Sleep(4 * time.Second)
+
+	pod, err := client.CoreV1().Pods("default").Get(ctx, "pod1", metav1.GetOptions{})
+	if err != nil {
+		return fmt.Errorf("get pod: %w", err)
+	}
+
+	klog.Info("scenario: pod1 is bound to " + pod.Spec.NodeName)
+
 	return nil
 }
