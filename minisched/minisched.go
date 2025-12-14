e3582e9f773892f90e7b03d9649a67b9971b5a46
Shinra Yamakawa <s.yamakawa@flatt.tech>
2025-12-14 12:34:26 +0900

    feat: implement scheduler

--- minisched/minisched.go
+++ minisched/minisched.go
@@ -2,10 +2,13 @@ package minisched
 
 import (
 	"context"
+	"math/rand"
+	"time"
 
-	"k8s.io/kubernetes/pkg/scheduler/framework"
+	"k8s.io/klog/v2"
 
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/OldBigBuddha/mini-kube-scheduler/minisched/queue"
 	"k8s.io/client-go/informers"
@@ -34,8 +37,39 @@ func (sched *Scheduler) Run(ctx context.Context) {
 }
 
 func (sched *Scheduler) scheduleOne(ctx context.Context) {
+	pod := sched.SchedulingQueue.NextPod()
+
+	nodes, err := sched.client.CoreV1().Nodes().List(ctx, metav1.ListOptions{})
+	if err != nil {
+		klog.Error(err)
+		return
+	}
+	klog.Info("minischeduler: Got Nodes successfully")
+
+	// pick a random node
+	rand.Seed(time.Now().UnixNano())
+	selectedNode := nodes.Items[rand.Intn(len(nodes.Items))]
+
+	// bind pod to node
+	if err := sched.Bind(ctx, pod, selectedNode.Name); err != nil {
+		klog.Error(err)
+		return
+	}
 }
 
-func (sched *Scheduler) Bind(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeName string) error {
+func (sched *Scheduler) Bind(ctx context.Context, p *v1.Pod, nodeName string) error {
+	binding := &v1.Binding{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: p.Namespace,
+			Name:      p.Name,
+			UID:       p.UID,
+		},
+	}
+
+	if err := sched.client.CoreV1().Pods(binding.Namespace).Bind(ctx, binding, metav1.CreateOptions{}); err != nil {
+		return err
+	}
+
+	klog.Infof("minischeduler: Pod %s/%s is bound to %s", p.Namespace, p.Name, nodeName)
 	return nil
 }
